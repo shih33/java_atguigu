c4bdd34032798d11eb37df59199e2be343d02990
shih33 <adsfqaz1@gmail.com>
2024-08-12 21:35:44 +0800

    Update and rename File類與IO流 to read_csv.java

---
+++ read_csv.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = new File("ETF日報表_20240701.csv");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String data;
+        long sum = 0;
+        data = br.readLine(); //讀取第一列的中文
+        while((data = br.readLine()) != null) {
+            String[] str = data.split("\"");
+            if(str[3].endsWith("B")) {
+                String noComma = str[9].replace("," , "");
+                long number = Long.parseLong(noComma);
+                sum += number;
+            }
+        }
+        System.out.println(sum);
+        //
+        br.close();
+    }
+}
