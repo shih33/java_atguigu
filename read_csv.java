113d8d8b6636d954aa256a3cabc0269d10f9dc06
shih33 <adsfqaz1@gmail.com>
2024-08-15 17:22:43 +0800

    Update read_csv.java

--- read_csv.java
+++ read_csv.java
@@ -1,24 +1,37 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.math.BigDecimal;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        File file = new File("ETF日報表_20240701.csv");
+/*
+ * 找出債券類ETF(基金代號是B結尾)的最新申報的總淨值合計。
+ * 建立ETF物件
+ */
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
-
-        String data;
-        long sum = 0;
-        data = br.readLine(); //讀取第一列的中文
-        while((data = br.readLine()) != null) {
-            String[] str = data.split("\"");
-            if(str[3].endsWith("B")) {
-                String noComma = str[9].replace("," , "");
-                long number = Long.parseLong(noComma);
-                sum += number;
-            }
-        }
-        System.out.println(sum);
-        //
-        br.close();
-    }
+public class readETF4 {
+	public static void main(String[] args) throws IOException {
+		//創建File物件
+		File file = new File("ETF日報表_20240701.csv");
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		String data;
+		ArrayList<String> etf_array = new ArrayList<String>(); //存儲讀取過的債券類ETF
+		BigDecimal sum = new BigDecimal("0");  //債券類ETF的最新申報的總淨值合計
+		ETF[] table = new ETF[12000];
+		int i = 0;
+		
+		data = br.readLine(); //讀取第一行的中文
+		while((data = br.readLine()) != null) {
+			String[] str = data.split("\"");  //以"分割data
+			table[i] = new ETF(str[1], str[3], str[5], str[7], str[9]); 
+			i++;		
+		}
+		for(int j=0; j < table.length; j++) {
+			System.out.println(table[j].getDate() + ' '+ table[j].getSymbol() + ' ' + table[j].getUnitNetValue()
+			+ ' ' + table[j].getIssuedUnits() + ' ' + table[j].getTotalNetValue());
+		}
+		
+		br.close();
+	}
 }
+
