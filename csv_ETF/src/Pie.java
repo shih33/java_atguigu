b50b0360d0a7cc1929a84b56f5c468e4d46f2452
shih33 <adsfqaz1@gmail.com>
2024-08-19 17:22:21 +0800

    Update and rename Output.java to Pie.java

---
+++ csv_ETF/src/Pie.java
@@ -0,0 +1,19 @@
+public class Pie {
+	private String dateTime;
+	private TotalAv totalAv;
+	
+	
+	public String getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+	public TotalAv getTotalAv() {
+		return totalAv;
+	}
+	public void setTotalAv(TotalAv totalAv) {
+		this.totalAv = totalAv;
+	}
+	
+}
