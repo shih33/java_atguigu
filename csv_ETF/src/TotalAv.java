f9d0081fb30b63fb86a6658c55a7286f5da94888
shih33 <adsfqaz1@gmail.com>
2024-08-19 17:21:09 +0800

    Update TotalAv.java

--- csv_ETF/src/TotalAv.java
+++ csv_ETF/src/TotalAv.java
@@ -1,6 +1,21 @@
-import java.math.BigDecimal;
+import java.util.List;
 
 public class TotalAv {
-    private BigDecimal total;
-
+	private double total;
+	private List<List<Object>> data;
+	
+	
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public List<List<Object>> getData() {
+		return data;
+	}
+	public void setData(List<List<Object>> data) {
+		this.data = data;
+	}
+	
 }
